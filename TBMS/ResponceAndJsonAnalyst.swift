2c81abecbb087d40776f83775e03994e76199984
chiao <aaron.ni1314@gmail.com>
2017-05-22 23:37:14 +0800

    更新ResponceAndJsonAnalyst.swift, 回傳JSON檔

--- TBMS/ResponceAndJsonAnalyst.swift
+++ TBMS/ResponceAndJsonAnalyst.swift
@@ -14,10 +14,7 @@ import SwiftyJSON
  1. 將回傳的JSON資料物件化
  
  未完成部分：
- 2. 解析模式切換 - steps內有三種資料結構：
- a. 捷運地鐵模式: transit_detail（dictionary）資料存在, 解析這層作為路線資訊
- b. 公車客運模式: steps (第二層steps, 也是array) 資料存在, 解析這層作為路線資訊
- c. 走路模式: steps內“transit_detail”與第二層“steps”均不存在, 以html_instructions作為路線資訊
+ 1. 各物件/屬性註解
  */
 
 class DirectionJsonAnalyst: NSObject {
@@ -98,7 +95,7 @@ class DirectionJsonAnalyst: NSObject {
             endLctLat = step[keyEndLocation][keyLat].double!
             endLctlng = step[keyEndLocation][keyLng].double!
             
-            let stepDetail = StepsData()
+            var stepDetail = StepsData()
             stepDetail.produceBasicPathObject( distance: distance,
                                                duration: duration,
                                                strAddress: nil,
@@ -107,93 +104,118 @@ class DirectionJsonAnalyst: NSObject {
                                                endLctLng: endLctlng,
                                                strLctLat: starLctLat,
                                                starLctLng: starLctlng )
-            
             // another step-parameters setting
             stepDetail.travelMode = step[keyTravelMode].string
             stepDetail.htmlInstructions = step[keyInstructions].string ?? ""
             stepDetail.maneuver = step[keyManeuver].string ?? ""
             
-            
-            
             if travelMode == "transit" {
-                // setting each step's detail
-                let transitDetailsData = step[keyTransitDetails]
                 
-                //                print(transitDetailsData) //測試用
-                //                print("///////////")
-                
-                
-                var tmpStopData = StopInformation()
-                tmpStopData.location = LocationInformation()
-                
-                // setting arrivalStop information
-                var tmpStopLct = step[keyTransitDetails][keyArrivalStop][keyLocation].dictionaryValue
-                tmpStopData.name = transitDetailsData[keyArrivalStop][keyName].string ?? ""
-                tmpStopData.location.latitude = tmpStopLct[keyLat]!.doubleValue
-                tmpStopData.location.longitude = tmpStopLct[keyLng]!.doubleValue
-                stepDetail.arrivalStop = tmpStopData
-                
-                // setting departureStop information
-                tmpStopLct = (transitDetailsData[keyDepartureStop][keyLocation].dictionaryValue)
-                tmpStopData.name = transitDetailsData[keyArrivalStop][keyName].string ?? ""
-                tmpStopData.location.latitude = tmpStopLct[keyLat]!.doubleValue
-                tmpStopData.location.longitude = tmpStopLct[keyLng]!.doubleValue
-                stepDetail.departureStop = tmpStopData
-                
-                // setting time information
-                stepDetail.arrivalTime = transitDetailsData[keyArrivalTime][keyText].string
-                stepDetail.departureTime = transitDetailsData[keyDepartureTime][keyText].string
-                
-                stepDetail.headsign = transitDetailsData[keyHeadsign].string
-                stepDetail.headway = transitDetailsData[keyHeadway].int
-                stepDetail.numbersOfStops = transitDetailsData[keyNumStops].int
-                
-                // setting line information
-                let tmpLineDetail = transitDetailsData[keyLine].dictionaryValue
-                print(transitDetailsData[keyLine])
-                
-                let agenciesData = tmpLineDetail[keyAgencies]!.arrayValue
-                stepDetail.lineAgencies = [String]()
-                for agency in agenciesData {
-                    stepDetail.lineAgencies.append(agency[keyName].stringValue)
+                if step[keySteps].array != nil {
+                    self.analyteBusModeResponseJSON(stepDetail: &stepDetail, steps:step[keySteps].arrayValue )
+                    
+                } else if step[keyTransitDetails].dictionary != nil {
+                    self.analyteSubwayModeResponseJSON(stepDetail: &stepDetail, step: step)
+                    
+                } else {
+                    self.analyteWalkingModeResponseJSON()
                 }
-                stepDetail.lineColor = tmpLineDetail[keyColor]!.string
-                stepDetail.lineShortame = tmpLineDetail[keyShortName]!.string
-                stepDetail.lineIcon = tmpLineDetail[keyVehicle]![keyIcon].string ?? ""
-                stepDetail.lineLocalIcon = tmpLineDetail[keyVehicle]![keyLocalIcon].string ?? ""
-                
-                //____________做測試囉～～～
-                print("transit測試囉")
-                testPrint(array: [stepDetail.arrivalStop.name,
-                                  stepDetail.departureStop.location.latitude,
-                                  stepDetail.arrivalTime,
-                                  stepDetail.departureTime,
-                                  stepDetail.headsign,
-                                  stepDetail.headway,
-                                  stepDetail.lineAgencies,
-                                  stepDetail.lineIcon,
-                                  stepDetail.lineShortame,
-                                  stepDetail.lineColor,
-                                  stepDetail.numbersOfStops])
-                
-                //--------------到這邊結束--------------------------------
             }
             legs.steps.append(stepDetail)
         }
         return legs
     }
-}
-// 測試用func : 測試物件屬性的值有被正確帶入
-private func testPrint(array:[Any?]){
-    var i = 0
-    for obj in array {
-        i += 1
-        if obj == nil {
-            print("\(i):    ")
-        } else {
-            print("\(i): \(obj!)")
+    
+    
+    func analyteWalkingModeResponseJSON () {
+        print("走路囉")
+    }
+    
+    func analyteBusModeResponseJSON (stepDetail:inout StepsData ,steps:[JSON]) {
+        
+        stepDetail.steps = [StepsData]()
+        for step in steps {
+            let distance = step[keyDistance][keyText].string!
+            let duration = step[keyDuration][keyText].string!
+            let starLctLat = step[keyStartLocation][keyLat].double!
+            let starLctlng = step[keyStartLocation][keyLng].double!
+            let endLctLat = step[keyEndLocation][keyLat].double!
+            let endLctlng = step[keyEndLocation][keyLng].double!
+            
+            let stepsData = SecondOrderStepsData()
+            stepsData.produceBasicPathObject( distance: distance,
+                                              duration: duration,
+                                              strAddress: nil,
+                                              endAddress: nil,
+                                              endLctLat: endLctLat,
+                                              endLctLng: endLctlng,
+                                              strLctLat: starLctLat,
+                                              starLctLng: starLctlng )
+            // another step-parameters setting
+            stepsData.travelMode = step[keyTravelMode].string
+            stepsData.htmlInstructions = step[keyInstructions].string ?? ""
+            stepDetail.steps.append(stepsData)
         }
     }
+    
+    func analyteSubwayModeResponseJSON (stepDetail:inout StepsData ,step:JSON) {
+        
+        // setting each step's detail
+        let transitDetailsData = step[keyTransitDetails]
+        
+        // setting arrivalStop information
+        var tmpStopLct = step[keyTransitDetails][keyArrivalStop][keyLocation].dictionaryValue
+        let arrivelStopData = StopInformation()
+        arrivelStopData.location = LocationInformation()
+        arrivelStopData.name = transitDetailsData[keyArrivalStop][keyName].string ?? ""
+        arrivelStopData.location.latitude = tmpStopLct[keyLat]!.doubleValue
+        arrivelStopData.location.longitude = tmpStopLct[keyLng]!.doubleValue
+        stepDetail.arrivalStop = arrivelStopData
+        
+        // setting departureStop information
+        let departureStopData = StopInformation()
+        departureStopData.location = LocationInformation()
+        tmpStopLct = (transitDetailsData[keyDepartureStop][keyLocation].dictionaryValue)
+        departureStopData.name = transitDetailsData[keyDepartureStop][keyName].string ?? ""
+        departureStopData.location.latitude = tmpStopLct[keyLat]!.doubleValue
+        departureStopData.location.longitude = tmpStopLct[keyLng]!.doubleValue
+        stepDetail.departureStop = departureStopData
+        
+        // setting time information
+        stepDetail.arrivalTime = transitDetailsData[keyArrivalTime][keyText].string
+        stepDetail.departureTime = transitDetailsData[keyDepartureTime][keyText].string
+        
+        stepDetail.headsign = transitDetailsData[keyHeadsign].string
+        stepDetail.headway = transitDetailsData[keyHeadway].int
+        stepDetail.numbersOfStops = transitDetailsData[keyNumStops].int
+        
+        // setting line information
+        let tmpLineDetail = transitDetailsData[keyLine].dictionaryValue
+        let agenciesData = tmpLineDetail[keyAgencies]!.arrayValue
+        
+        stepDetail.lineAgencies = [String]()
+        for agency in agenciesData {
+            stepDetail.lineAgencies.append(agency[keyName].stringValue)
+        }
+        stepDetail.lineColor = tmpLineDetail[keyColor]!.string
+        stepDetail.lineShortame = tmpLineDetail[keyShortName]!.string
+        stepDetail.lineIcon = tmpLineDetail[keyVehicle]![keyIcon].string ?? ""
+        stepDetail.lineLocalIcon = tmpLineDetail[keyVehicle]![keyLocalIcon].string ?? ""
+    }
+    
+    //    // 測試用func : 測試物件屬性的值有被正確帶入
+    //    private func testPrint(array:[Any?]){
+    //        var i = 0
+    //        for obj in array {
+    //            i += 1
+    //            if obj == nil {
+    //                print("\(i):    ")
+    //            } else {
+    //                print("\(i): \(obj!)")
+    //            }
+    //        }
+    //    }
+    
 }
 
 // 撰寫Error情況
@@ -224,14 +246,14 @@ class GeneralDirectionData {
     var endLocation = LocationInformation()
     var startLocation = LocationInformation()
     
-    func produceBasicPathObject ( distance:String,
-                                  duration:String,
-                                  strAddress:String!,
-                                  endAddress:String!,
-                                  endLctLat:Double,
-                                  endLctLng:Double,
-                                  strLctLat:Double,
-                                  starLctLng:Double ) {
+    fileprivate func produceBasicPathObject ( distance:String,
+                                              duration:String,
+                                              strAddress:String!,
+                                              endAddress:String!,
+                                              endLctLat:Double,
+                                              endLctLng:Double,
+                                              strLctLat:Double,
+                                              starLctLng:Double ) {
         self.distance = distance
         self.duration = duration
         
@@ -255,12 +277,18 @@ class LegsData: GeneralDirectionData {
     var departureTime : TimeInformation!
 }
 
-// steps
-class StepsData: GeneralDirectionData {
+class SecondOrderStepsData: GeneralDirectionData {
     var travelMode : String!
     var htmlInstructions : String!  //路程簡介
+}
+
+// steps
+class StepsData: SecondOrderStepsData {
+    
     var maneuver : String!
     var polyline : [String:String]!
+    
+    var steps : [SecondOrderStepsData]!
     
     //大眾運輸時路程細節
     //    var transitDetail : TransitDetails!
