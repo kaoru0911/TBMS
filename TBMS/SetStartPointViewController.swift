d8cb9c196c980847b63722bc8739d53f0ff3e790
chiao <aaron.ni1314@gmail.com>
2017-05-24 20:14:05 +0800

    更新增加景點＆起點的傳值

--- TBMS/SetStartPointViewController.swift
+++ TBMS/SetStartPointViewController.swift
@@ -7,44 +7,74 @@
 //
 
 import UIKit
-//import GooglePlaces
 import GooglePlacePicker
 
 class SetStartPointViewController: UIViewController {
     
+    @IBOutlet weak var chosenStartingPoint: UILabel!
     @IBOutlet weak var startingPointText: UITextField!
+    @IBOutlet weak var chooseStartingPtBtn: UIButton!
+    @IBOutlet weak var goToNextPage: UIButton!
+    
+    var startPoint : Attraction!
+    var attractionsList : [Attraction]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        goToNextPage.isHidden = true
+        chosenStartingPoint.isHidden = true
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func btn(_ sender: Any) {
+    
+    @IBAction func chooseStartingPoint(_ sender: UIButton) {
+        
         let config = GMSPlacePickerConfig(viewport: nil)
         let placePicker = GMSPlacePicker(config: config)
         
         placePicker.pickPlace(callback: { (place, error) -> Void in
+            
             if let error = error {
                 print("Pick Place error: \(error.localizedDescription)")
                 return
             }
-            
             guard let place = place else {
                 print("No place selected")
                 return
             }
             
-            self.startingPointText.text = place.name
+            self.chosenStartingPoint.text = place.name
+            self.chooseStartingPtBtn.titleLabel?.text = "重新選擇"
+            
+            self.chosenStartingPoint.isHidden = false
+            self.goToNextPage.isHidden = false
+            
+            self.startPoint.setValueToAttractionObject(place: place)
         })
-
     }
-    
-    @IBAction func inputStartPoint(_ sender: Any) {
+    @IBAction func goToNextPage(_ sender: Any) {
         
-            }
+    }
+}
+
+struct Attraction {
+    
+    var attrctionName : String!
+    var placeID : String!
+    var coordinate : CLLocationCoordinate2D!
+    var address : String?
+    var phoneNumber : String?
+    
+    mutating func setValueToAttractionObject (place:GMSPlace) {
+        
+        attrctionName = place.name
+        coordinate = place.coordinate
+        placeID = place.placeID
+        
+        if let phoneNumber = place.phoneNumber { self.phoneNumber = phoneNumber }
+        if let address = place.formattedAddress { self.address = address }
+    }
 }
