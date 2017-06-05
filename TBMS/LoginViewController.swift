020da41997a29e2c0072b11d524cfa9014c72682
Ryder <kaoru0911@gmail.com>
2017-06-05 12:12:59 +0800

    FB登入改頁

--- TBMS/LoginViewController.swift
+++ TBMS/LoginViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 import Alamofire
+import FBSDKLoginKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     @IBOutlet weak var inputPassword: UITextField!
     @IBOutlet weak var inputAccountName: UITextField!
     
+    @IBOutlet weak var FBLoginBtn: FBSDKLoginButton!
+   
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var newMemberRegisterBtn: UIButton!
     
@@ -28,6 +31,15 @@ class LoginViewController: UIViewController {
         loginBtn.layer.cornerRadius = 5.0
         newMemberRegisterBtn.layer.cornerRadius = 5.0
         
+        FBLoginBtn.readPermissions = ["public_profile", "email", "user_friends"]
+        FBLoginBtn.delegate = self
+        
+        //  FB第一次登入後可取得使用者token，後續即可直接登入
+        if (FBSDKAccessToken.current()) != nil{
+            //            fetchProfile()
+        }
+
+        
         NotificationCenter.default.addObserver(self, selector: #selector(NotificationDidGet), name: NSNotification.Name(rawValue: "loginNotifier"), object: nil)
     }
 
@@ -36,6 +48,55 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+        print("成功登入")
+        
+        fetchProfile()
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    func fetchProfile(){
+        print("fetch profile")
+        
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start(completionHandler: {
+            connection, result, error -> Void in
+            
+            if error != nil {
+                print("longinerror =\(String(describing: error))")
+            } else {
+                
+                if let resultNew = result as? [String:Any]{
+                    
+                    let email = resultNew["email"]  as! String
+                    print(email)
+                    
+                    let firstName = resultNew["first_name"] as! String
+                    print(firstName)
+                    
+                    let lastName = resultNew["last_name"] as! String
+                    print(lastName)
+                    
+                    if let picture = resultNew["picture"] as? NSDictionary,
+                        let data = picture["data"] as? NSDictionary,
+                        let url = data["url"] as? String {
+                        print(url) //臉書大頭貼的url, 再放入imageView內秀出來
+                    }
+                }
+            }
+        })
+    }
+
 
     /*
     // MARK: - Navigation
